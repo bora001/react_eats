f9159a955563a0ee1bd628d2d4a7e855fb141803
unknown <bora.vancouver@gmail.com>
2022-03-25 16:49:32 +0900

    add addBtn

---
+++ src/components/Menu/AddBtn.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./AddBtn.css";
+const AddBtn = () => {
+  return (
+    <div>
+      <button>+ ADD</button>
+    </div>
+  );
+};
+
+export default AddBtn;
--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -1,11 +1,15 @@
 import React from "react";
+import AddBtn from "./AddBtn";
 import "./MenuItem.css";
 const MenuItem = (props) => {
   return (
-    <div className="menu_item">
-      <p className="menu_name">{props.data.name}</p>
-      <p className="menu_desc">{props.data.desc}</p>
-      <p className="menu_price">$ {props.data.price}</p>
+    <div className="menu_box">
+      <div className="menu_item">
+        <p className="menu_name">{props.data.name}</p>
+        <p className="menu_desc">{props.data.desc}</p>
+        <p className="menu_price">$ {props.data.price}</p>
+      </div>
+      <AddBtn />
     </div>
   );
 };
