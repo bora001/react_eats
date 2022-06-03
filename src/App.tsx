c1df55f0a9c213f99918cd11dbd542e9ebf27e85
unknown <bora.vancouver@gmail.com>
2022-06-03 14:19:50 +0900

    page setting

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,25 @@ import Header from "./components/Layout/Header";
 import MenuList from "./components/Menu/MenuList";
 import MyOrder from "./components/MyOrder/MyOrder";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { useSelector } from "react-redux";
 import { useAppSelector } from "./store/hooks";
 
 const App = () => {
   const cartInfo = useAppSelector((state) => state.cart);
-  // const cartInfo = useSelector((state) => state.cart);
 
   return (
     <BrowserRouter>
       <div>
-        {/* {cartInfo.modalStatus === "Login" && <LoginPage />}
-      {cartInfo.modalStatus === "Register" && <RegisterPage />}
-      {cartInfo.modalStatus === "Cart" && <CartPage />}
-      {cartInfo.cartStatus && <CartPage />} */}
+        {cartInfo.modalStatus === "Login" && <LoginPage />}
+        {cartInfo.modalStatus === "Register" && <RegisterPage />}
+        {cartInfo.modalStatus === "Cart" && <CartPage />}
+        {cartInfo.cartStatus && <CartPage />}
         <Header />
-        {/* <div className="main_box">
-        <Routes>
-          <Route path="/" element={<MenuList />} exact />
-          <Route path="/myorder" element={<MyOrder />} exact />
-        </Routes>
-      </div> */}
+        <div className="main_box">
+          <Routes>
+            <Route path="/" element={<MenuList />} />
+            <Route path="/myorder" element={<MyOrder />} />
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );
